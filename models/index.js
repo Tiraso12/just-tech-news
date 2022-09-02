2c466eba523d88aa30a26d45e4bda5e5d371b4c3
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-09-01 21:28:39 -0400

    votes added

--- models/index.js
+++ models/index.js
@@ -1,15 +1,16 @@
-const User = require("./User");
-const Post = require("./Post");
+// import all models
+const Post = require('./Post');
+const User = require('./User');
 const Vote = require('./Vote');
 
 // create associations
 User.hasMany(Post, {
-    foreignKey: 'user_id'
-  });
+  foreignKey: 'user_id'
+});
 
-Post.belongsTo(User,{
-    foreignKey: 'user_id',
-})
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
 
 User.belongsToMany(Post, {
   through: Vote,
